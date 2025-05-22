2d8d4cda50c2763735b9aa0c1a9fc4d82525b893
Nate <nathanael.paul.lewis@gmail.com>
2025-05-22 00:31:08 -0500

    Updated files on 2025-05-22

--- files/wezterm/nxstynate/background.lua
+++ files/wezterm/nxstynate/background.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 function M.apply(config)
-	local bg_image = os.getenv("HOME") .. "/dotfiles/wezterm/nxstynate/images/background.jpg"
+	-- local bg_image = os.getenv("HOME") .. "/pro-env/files/wezterm/nxstynate/images/background-01.jpg"
+	local bg_image = os.getenv("HOME") .. "/pro-env/files/wezterm/nxstynate/images/background-02.jpg"
 
 	config.window_background_image = bg_image
 	config.window_background_image_hsb = {
-		brightness = 0.04, -- 0 = black, 1 = full brightness
+		brightness = 0.015, -- 0 = black, 1 = full brightness
 		hue = 1.0,
 		saturation = 0.5,
 	}
